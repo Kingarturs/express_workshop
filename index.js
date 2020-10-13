5ca2a72c19ca2b6d74cfd21ed7ecfedb3f9b9b53
Luis Arturo García Gonzalez <lgarcia103@alumnos.uaq.mx>
2020-10-12 22:26:07 -0500

    Added bodyParser and made a POST example

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const express = require('express')
-const app = express()
-const { pokemon } = require('./pokedex.json')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { pokemon } = require('./pokedex.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al pokedex");
+    return res.status(200).send("Bienvenido al pokedex");
 })
 
-app.get('/pokemon/all', (req, res, next) => {
-    res.status(200)
-    res.send(pokemon)
+app.post('/pokemon', (req, res, next) => {
+    return res.status(200).send(req.body);
+})
+
+app.get('/pokemon', (req, res, next) => {
+    return res.status(200).send(pokemon);
 })
 
 app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
     const id = req.params.id - 1;
-    if (id >= 0 && id <= 150){
-        res.status(200);
-        return res.send(pokemon[req.params.id - 1]);
-    } else {
-        res.status(404);
-        res.send("Pokemón no encontrado");  
-    }
-    
+
+    return (id >= 0 && id <= 150) ? 
+        res.status(200).send(pokemon[req.params.id - 1]) : 
+        res.status(404).send("Pokemón no encontrado"); 
 })
 
-app.get('/pokemon/:name', (req, res, next) => {
+app.get('/pokemon/:name([A-Za-z]*)', (req, res, next) => {
     const name = req.params.name;
     
-    for(i = 0; i < pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            return res.send(pokemon[i]);
-        }
-    }
+    const pk = pokemon.filter((p) => {
+        // Condición a evaluar ? valor si verdadero: valor si falso
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    })
 
-    res.status(404);
-    res.send("Pokemón no encontrado");
+    return (pk.length > 0) ? 
+        res.status(200).send(pk) :
+        res.status(404).send("Pokemón no encontrado");
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running')
+    console.log('Server is running');
 })
