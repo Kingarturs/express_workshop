c93f5a06ff0c1d77fcc454f722ed58b4a83de875
Luis Arturo García Gonzalez <lgarcia103@alumnos.uaq.mx>
2020-10-20 09:09:04 -0500

    Removed body-parser and made a 404 response

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,12 +3,12 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 pokemon.post('/', (req, res, next) => {
-    return res.status(200).send(req.body);
+    return res.status(200).json(req.body);
 })
 
 pokemon.get('/', async(req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(pkmn);
+    return res.status(200).json({ code: 1, massage: pkmn });
 })
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
@@ -16,10 +16,10 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
 
     if (id >= 1 && id <= 722) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=?", id);
-        return res.status(200).json(pkmn)
+        return res.status(200).json({ code: 1, massage: pkmn })
     }
 
-    return res.status(404).send("Pokemón no encontrado"); 
+    return res.status(404).send({ code: 404, message: "Pokemón no encontrado" }); 
 })
 
 pokemon.get('/:name([A-Za-z]*)', async(req, res, next) => {
@@ -27,11 +27,11 @@ pokemon.get('/:name([A-Za-z]*)', async(req, res, next) => {
     
     const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name=?", name, (err, rows) => {
         if (rows.lenght >= 1) {
-            return res.status(200).json(pkmn)
+            return res.status(200).json({ code: 1, massage: pkmn })
         }
     });
     
-    return res.status(404).send("Pokemón no encontrado"); 
+    return res.status(404).send({ code: 404, message: "Pokemón no encontrado" }); 
 })
 
 module.exports = pokemon;
